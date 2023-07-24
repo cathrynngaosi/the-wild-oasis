ffc69d1214d4933046b49a1079cac4c1f2e660cb
Cathryn Ngaosi <cathrynngaosi@gmail.com>
2023-07-24 18:53:11 +0800

    Implement uploading new cabin images to supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://hootiwusegdfyvplculm.supabase.co";
+export const supabaseUrl = "https://hootiwusegdfyvplculm.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhvb3Rpd3VzZWdkZnl2cGxjdWxtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTAxMjEwMDIsImV4cCI6MjAwNTY5NzAwMn0.QsS9OR3keIriid2G2SmbFqOqmJwgzzt1CfvS3eE7KZU";
 const supabase = createClient(supabaseUrl, supabaseKey);
