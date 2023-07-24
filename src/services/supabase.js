5c7b6f1320ac15ff63ea4b8baa6a93b37c6b9d85
Cathryn Ngaosi <cathrynngaosi@gmail.com>
2023-07-24 16:34:13 +0800

    Fetch cabin data

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://hootiwusegdfyvplculm.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhvb3Rpd3VzZWdkZnl2cGxjdWxtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTAxMjEwMDIsImV4cCI6MjAwNTY5NzAwMn0.QsS9OR3keIriid2G2SmbFqOqmJwgzzt1CfvS3eE7KZU";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
