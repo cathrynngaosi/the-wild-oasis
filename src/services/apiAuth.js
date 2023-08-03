2be071e6ced86c1416f924c831d6e4ba4e22206b
Cathryn Ngaosi <cathrynngaosi@gmail.com>
2023-08-03 15:25:33 +0800

    Add authentication: protect routes

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -11,3 +11,15 @@ export async function login({ email, password }) {
   console.log(data);
   return data;
 }
+
+export async function getUser() {
+  const { data: session } = await supabase.auth.getSession();
+  if (!session.session) return null;
+
+  const { data, error } = await supabase.auth.getUser();
+  console.log(data);
+
+  if (error) throw new Error(error.message);
+
+  return data?.user;
+}
